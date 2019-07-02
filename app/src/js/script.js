eea30c3809fb3620cb555a5f4cec541b78720d43
Chandu4221 <chndraja@gmail.com>
2019-07-03 01:15:08 +0530

    basic responsive dashboard template created

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,1 +1,8 @@
-/*custom javascript*/
+$(document).ready(function() {
+  $(".sideTrigger").on("click", function() {
+    $(".sidebar").toggleClass("active");
+  });
+  $(".menuTrigger").on("click", function() {
+    $(".nav-menu").animate({ height: "toggle" });
+  });
+});
